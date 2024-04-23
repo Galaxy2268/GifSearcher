30503f89925269cc73fe8d9b9d605ff2b317d964
Galaxy2268 <danielsmulukinss@gmail.com>
2024-04-23 21:58:30 +0300

    made simple remote interface

--- app/src/main/java/com/galaxy/gifsearcher/giflist/data/remote/GiphApi.kt
+++ app/src/main/java/com/galaxy/gifsearcher/giflist/data/remote/GiphApi.kt
@@ -1,15 +1,21 @@
 package com.galaxy.gifsearcher.giflist.data.remote
 
+import com.galaxy.gifsearcher.giflist.data.remote.util.GiphyKey.GIPHY_KEY
+import com.galaxy.gifsearcher.giflist.domain.model.Giph
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 
 interface GiphApi {
-    @GET
-    suspend fun getTrending()
+    @GET("trending?api_key=$GIPHY_KEY")
+    suspend fun getTrending(
+        @Query("offset") offset: Int
+    ): List<Giph>
 
-    @GET
+    @GET("search?api_key=$GIPHY_KEY")
     suspend fun getWithSearch(
-
-    )
+        @Query("q") query: String,
+        @Query("offset") offset: Int
+    ): List<Giph>
 
 }
