ed00be06e72ec9fce52c532794bae9042da73d87
Galaxy2268 <danielsmulukinss@gmail.com>
2024-05-21 11:33:33 +0300

    fixed views

--- app/src/main/java/com/galaxy/gifsearcher/giflist/presentation/components/GifCard.kt
+++ app/src/main/java/com/galaxy/gifsearcher/giflist/presentation/components/GifCard.kt
@@ -35,7 +35,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
-import coil.decode.ImageDecoderDecoder
+import coil.decode.GifDecoder
 import coil.request.ImageRequest
 import com.galaxy.gifsearcher.giflist.domain.model.Gif
 
@@ -90,7 +90,7 @@ fun GifCard(
                     .data(gif.url)
                     .placeholderMemoryCacheKey(gif.id)
                     .memoryCacheKey(gif.id)
-                    .decoderFactory(ImageDecoderDecoder.Factory())
+                    .decoderFactory(GifDecoder.Factory())
                     .build(),
                 contentDescription = "Gif",
                 modifier = Modifier
